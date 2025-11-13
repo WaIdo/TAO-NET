a1574f38741772ac79628131f9fbef8a7c78374a
WaIdo <36703533+WaIdo@users.noreply.github.com>
2025-11-13 13:33:29 +0800

    Add files via upload

---
+++ stage2_pacrep_vpn/bash_train_vpn.sh
@@ -0,0 +1,1 @@
+nohup python run_train.py --batch_size 8 --max_checkpoint 290 --breakpoint -1 --name_model att_VPN_addvocab --learning_rate 1e-5 --data_dir ./data/Data_of_VPN > att_Data_of_VPN.out 2>&1 &
