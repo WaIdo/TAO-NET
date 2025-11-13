b057882862f56659ab0de54fe47612126b9e5545
WaIdo <36703533+WaIdo@users.noreply.github.com>
2025-11-13 13:30:33 +0800

    Add files via upload

---
+++ stage2_pacrep_nontor/bash_train_Nontor.sh
@@ -0,0 +1,1 @@
+nohup python run_train.py --batch_size 8 --max_checkpoint 290 --breakpoint -1 --name_model att_Nontor_addvocab --learning_rate 1e-5 --data_dir ./data/Data_of_Nontor > att_Data_of_Nontor.out 2>&1 &
---
+++ stage2_pacrep_tsinghua/bash_train_tinghua.sh
@@ -0,0 +1,1 @@
+nohup python run_train.py --batch_size 8 --max_checkpoint 290 --breakpoint -1 --name_model att_tinghua_addvocab --learning_rate 1e-5 --data_dir ./data/tinghua > att_tinghua.out 2>&1 &
